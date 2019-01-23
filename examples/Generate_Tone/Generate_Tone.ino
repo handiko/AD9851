c95eccd51ffefe8f4c39855db2e6c8367a33ff6d
handiko <handikogesang@gmail.com>
2019-01-23 07:34:02 +0700

    Update Examples

--- examples/Generate_Tone/Generate_Tone.ino
+++ examples/Generate_Tone/Generate_Tone.ino
@@ -14,7 +14,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-#include <DDS.h>
+#include <AD9851.h>
 
 #define RST   8
 #define DATA  9
